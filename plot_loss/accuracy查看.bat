259829fb94ad7cb2e16704ea76105b962f3e28aa
lli27 <190072358@qq.com>
2018-10-18 23:16:23 +0800

    Add files via upload

---
+++ plot_loss/accuracy查看.bat
@@ -0,0 +1,3 @@
+@echo off
+python accuracy.py 0  D:\caffe\caffe-master\log\plot_loss\plot_accuracy\accuracy.png lenet.log
+
---
+++ plot_loss/loss查看.bat
@@ -0,0 +1,3 @@
+@echo off
+python loss.py 0  D:\caffe\caffe-master\log\plot_loss\plot_loss\loss1.png train.log
+pause
